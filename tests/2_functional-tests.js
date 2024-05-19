3079c5fc16213968f6b34559934670e20ca80e08
João Vítor <joao.paes@philips.com>
2024-05-19 20:54:22 +0000

    added main test structure

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -1,10 +1,76 @@
-const chaiHttp = require('chai-http');
-const chai = require('chai');
+const chaiHttp = require("chai-http");
+const chai = require("chai");
+const suite = require("mocha").suite;
+const mocha = require("mocha");
 const assert = chai.assert;
-const server = require('../server');
+const server = require("../server.js");
 
 chai.use(chaiHttp);
 
-suite('Functional Tests', function() {
+describe("Functional Tests", function () {
+  it("Viewing one stock: GET request to /api/stock-prices/", function (done) {
+    chai
+      .request(server)
+      .get("/api/stock-prices/")
+      .query({ stock: "GOOG" })
+      .end(function (err, res) {
+        assert.equal(res.status, 200);
+        assert.equal(res.body.stockData.stock, "GOOG");
+        done();
+      });
+  });
 
+  it("Viewing one stock and liking it: GET request to /api/stock-prices/", function (done) {
+    chai
+      .request(server)
+      .get("/api/stock-prices/")
+      .query({ stock: "GOOG" })
+      .end(function (err, res) {
+        assert.equal(res.status, 200);
+        assert.equal(res.body.stockData.stock, "GOOG");
+        done();
+      });
+  });
+
+  it("Viewing the same stock and liking it again: GET request to /api/stock-prices/", function (done) {
+    chai
+      .request(server)
+      .get("/api/stock-prices/")
+      .query({ stock: "GOOG" })
+      .end(function (err, res) {
+        assert.equal(res.status, 200);
+        assert.equal(res.body.stockData.stock, "GOOG");
+        done();
+      });
+  });
+
+  it("Viewing two stocks: GET request to /api/stock-prices/", function (done) {
+    chai
+      .request(server)
+      .get("/api/stock-prices/")
+      .query({ stock: "GOOG" })
+      .end(function (err, res) {
+        assert.equal(res.status, 200);
+        // assert.equal(res.body.stockData[0].stock, "GOOG");
+        done();
+      });
+  });
+
+  it("Viewing two stocks and liking them: GET request to /api/stock-prices/", function (done) {
+    chai
+      .request(server)
+      .get("/api/stock-prices/")
+      .query({ stock: "GOOG" })
+      .end(function (err, res) {
+        assert.equal(res.status, 200);
+        assert.equal(res.body.stockData.stock, "GOOG");
+        done();
+      });
+  });
+  /**
+   * Viewing one stock and liking it: GET request to /api/stock-prices/
+    Viewing the same stock and liking it again: GET request to /api/stock-prices/
+    Viewing two stocks: GET request to /api/stock-prices/
+    Viewing two stocks and liking them: GET request to /api/stock-prices/
+   */
 });
